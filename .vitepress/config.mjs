6fbb4eb36e9953d56fe152dc3e56f0274f4a1cb9
lixinzhang-cn <86455292@qq.com>
2024-06-12 16:07:38 +0800

    修改文章更新时间

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   base:'/blog/',
   title: "My Awesome Project",
   description: "A VitePress Site",
-  lastUpdated: '上次更新',  // 最后更新时间 string | boolean
+  lastUpdated: '上次更新', // string | boolean
   // 代码块启用行号
   markdown: {
     lineNumbers: true
